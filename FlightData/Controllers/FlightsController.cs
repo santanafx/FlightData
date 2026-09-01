7a777e96e695888ac9f1c955286a4a228ac42794
lucas <santanafx@hotmail.com>
2026-09-01 10:32:51 -0300

    feat(flights): add FlightsController with CRUD

---
+++ FlightData/Controllers/FlightsController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+//example: http://localhost:5609/api/flight/
+public class FlightsController : ControllerBase
+{
+  private readonly IFlightService _flightService;
+  public FlightsController(IFlightService flightService)
+  {
+    _flightService = flightService;
+  }
+
+  [HttpGet]
+  public ActionResult<List<Flight>> GetAllFlights()
+  {
+    return Ok(_flightService.GetAllFlights());
+  }
+
+  [HttpGet("{id}")]
+  public ActionResult<Flight> GetFlightById(int id)
+  {
+    var result = _flightService.GetFlightById(id);
+
+    if (result == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(result);
+  }
+
+  [HttpPost]
+  public ActionResult<Flight> CreateFlight(Flight flight)
+  {
+    return Ok(_flightService.CreateFlight(flight));
+  }
+
+  [HttpPut("{id}")]
+  public ActionResult<Flight> UpdateFlight(int id, Flight updatedFlight)
+  {
+    var result = _flightService.UpdateFlight(id, updatedFlight);
+
+    if (result == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(result);
+  }
+
+  [HttpDelete("{id}")]
+  public ActionResult<string> DeleteFlight(int id)
+  {
+    var result = _flightService.DeleteFlight(id);
+
+    if (result == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(result);
+  }
+}
